1affd81a31a7b3ed06e5f6125aa5af5d5c8494c7
Axel Yung <axelyung@gmail.com>
2021-12-02 12:44:44 -0800

    basic table setup

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = function () {
   const onMessage = ({ data }: MessageEvent) => {
     const { id, exchange, price, quantity } = JSON.parse(data).payload;
     console.log(JSON.parse(data));
-    setTrades((ts) => [...ts, { id, exchange, price, quantity }]);
+    setTrades((ts) => [{ id, exchange, price, quantity }, ...ts.slice(0, 99)]);
     console.log({ id, exchange });
   };
   useEffect(() => {
@@ -40,13 +40,57 @@ const App = function () {
     };
   }, []);
   return (
-    <div className="App">
+    <div className="App container mx-auto">
       <div>trades ({trades.length})</div>
-      {trades.map((t) => (
-        <div key={t.id}>
-          {t.id} - {t.exchange} - {t.quantity} - {t.price}
+      <div className="flex flex-col">
+        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Exchange
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Quantity
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Price
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {trades.map((t) => (
+                    <tr key={t.id}>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        {t.exchange}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        {t.quantity}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">{t.price}</td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        Admin
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium" />
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
-      ))}
+      </div>
     </div>
   );
 };
