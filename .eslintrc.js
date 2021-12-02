2d8e060e66439ca0dee9b64b54347639d86d9d2a
Axel Yung <axelyung@gmail.com>
2021-12-02 11:14:24 -0800

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+  },
+  parser: `@typescript-eslint/parser`,
+  plugins: [`@typescript-eslint`, `prettier`],
+  extends: [
+    `eslint:recommended`,
+    `airbnb`,
+    `airbnb/hooks`,
+    `plugin:prettier/recommended`,
+  ],
+  rules: {
+    quotes: [2, `backtick`],
+    'prettier/prettier': [2, { singleQuote: true }],
+    'react/jsx-filename-extension': [1, { extensions: [`.tsx`] }],
+    'no-use-before-define': 0,
+    '@typescript-eslint/no-use-before-define': 2,
+  },
+};
